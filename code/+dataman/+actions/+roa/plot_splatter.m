cbae94773e750dc18e699f1e33d19ff43f9573d3
Knutsi <knutsindre@gmail.com>
2021-04-15 17:45:30 +0200

    Function to downsample and plot 3d splatter plots from dataman (with default params)

---
+++ code/+dataman/+actions/+roa/plot_splatter.m
@@ -0,0 +1,28 @@
+function plot_splatter(ts, model, editor)
+    def_conf = struct();
+    
+    def_conf.start_f = 1;
+    def_conf.end_f = ts.frame_count;
+    def_conf.ds_x = 10;
+    def_conf.ds_y = 10;
+    def_conf.ds_z = 100;
+
+    c = ts.load_var("splatter_config", def_conf);
+    
+    % downsample and plot each channel:
+    for chan_idx = 1:ts.channels
+        chan = "ch" + chan_idx;
+        mask_var = "roa_mask_" + chan;
+        if ~ts.has_var(mask_var); continue; end
+        
+        mask = ts.load_var(mask_var);
+        x_range = 1:c.ds_x:ts.img_dim(1);
+        y_range = 1:c.ds_y:ts.img_dim(2);
+        z_range = c.start_f:c.ds_z:c.end_f;
+        
+        mask_ds = mask(x_range, y_range, z_range);
+        begonia.processing.roa.plot_roa_3d(mask_ds, ts.dx, ts.dy, ts.dt)   
+    end
+
+end
+
