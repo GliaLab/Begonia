06de7009f50baef18c1c03a8baaefc019f51eb87
Mario <mariofe@uio.no>
2021-04-18 12:12:22 +0200

    create new uuids when copy/paste roi_table

--- code/+xylobium/+dledit/+actions/paste_to_all.m
+++ code/+xylobium/+dledit/+actions/paste_to_all.m
@@ -1,17 +1,21 @@
 function paste_to_all(dlocs, model, editor)
-    import begonia.util.to_loopable;
-    
-    if isempty(editor.clipboard_value)
-        msgbox('Copy a value first');
-        return;
-    end
-    
-    for dloc = to_loopable(dlocs)
-        for var = model.selected_vars
-            cvar = char(var);
-            model.save(dloc, cvar, editor.clipboard_value);
-        end
+import begonia.util.to_loopable;
 
-    end
+if isempty(editor.clipboard_value)
+    msgbox('Copy a value first');
+    return;
+end
+
+for dloc = to_loopable(dlocs)
+    for var = model.selected_vars
+        cvar = char(var);
+        if var == "roi_table"
+            n_rois = height(editor.clipboard_value);
+            uuids = string(begonia.util.make_uuids(n_rois));
+            editor.clipboard_value.roi_id = uuids;
+        end
+        model.save(dloc, cvar, editor.clipboard_value);
+    end  
+end
 end
 
