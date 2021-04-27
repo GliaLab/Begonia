106fa1db9ae00137aefe51e78b4f5e549d43211b
Mario <mariofe@uio.no>
2021-04-27 16:47:15 +0200

    change roi table source when copy/paste

--- code/+xylobium/+dledit/+actions/paste_to_all.m
+++ code/+xylobium/+dledit/+actions/paste_to_all.m
@@ -1,11 +1,11 @@
 function paste_to_all(dlocs, model, editor)
 import begonia.util.to_loopable;
-
+ 
 if isempty(editor.clipboard_value)
     msgbox('Copy a value first');
     return;
 end
-
+ 
 for dloc = to_loopable(dlocs)
     for var = model.selected_vars
         cvar = char(var);
@@ -13,9 +13,11 @@ for dloc = to_loopable(dlocs)
             n_rois = height(editor.clipboard_value);
             uuids = string(begonia.util.make_uuids(n_rois));
             editor.clipboard_value.roi_id = uuids;
+            source_id = repmat(string(dloc.name),n_rois,1);
+            editor.clipboard_value.source_id = source_id;
         end
         model.save(dloc, cvar, editor.clipboard_value);
     end  
 end
 end
-
+ 
