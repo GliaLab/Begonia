cf83e263fea33b49d0e8be59abff475e3579b9c4
lastis <neitakkikkebanan@gmail.com>
2021-06-08 14:56:01 +0200

    Fix file ending of previous commit

--- code/+begonia/+processing/+tseries_migration/save_as_tiff.m
+++ code/+begonia/+processing/+tseries_migration/save_as_tiff.m
@@ -8,6 +8,9 @@ ts = begonia.scantype.find_scans(folder);
 for i = 1:length(ts)
     
     new_ts_path = strrep(ts(i).path,folder,folder_out);
+    if ~new_ts_path.endsWith(".tif")
+        new_ts_path = new_ts_path + ".tif";
+    end
     
     begonia.scantype.tiff.tseries_to_tiff(ts(i),new_ts_path,merged_frames);
 end
