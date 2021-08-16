56bd6c7d97d9219c44a683824c9ebc94d225511f
Knut <knutsindre@gmail.com>
2021-08-16 17:06:56 +0200

    Utility method to re-read H5Old metadata from bundled XML file

---
+++ code/+begonia/+scantype/+h5_old/reread_xml_metadata.m
@@ -0,0 +1,23 @@
+function reread_xml_metadata(tseries)
+
+    for ts = begonia.util.to_loopable(tseries)
+        asdata
+        if ~isa(ts, "begonia.scantype.h5_old.TSeriesH5Old")
+            error("TSeries must be H5Old format for this method to work")
+        end
+        
+        xml_file = fullfile(ts.path, ts.name + ".xml");
+        metadata = begonia.scantype.prairie.read_metadata(xml_file);
+
+        ts.channel_names        = metadata.channel_names;
+        ts.channels             = metadata.channels;
+        ts.dt                   = metadata.dt;
+        ts.dx                   = metadata.dx;
+        ts.dy                   = metadata.dx;
+        ts.cycles               = metadata.cycles;
+        ts.zoom                 = metadata.optical_zoom;
+        ts.frame_count          = metadata.frames_in_cycle;
+        ts.frame_position_um    = metadata.frame_position_um;
+    end
+end
+
